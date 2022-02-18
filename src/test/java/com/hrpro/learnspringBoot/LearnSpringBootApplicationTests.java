4d87e684239f66f186e9c16add12ad9b8de3d561
abdelhalim <abdelhalimpro@gmail.com>
2022-02-18 22:57:53 +0100

    initial commit

---
+++ src/test/java/com/hrpro/learnspringBoot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrpro.learnspringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
