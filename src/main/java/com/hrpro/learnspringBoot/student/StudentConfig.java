53ed315f56c3764503d93a2d02df3b0756c18f21
abdelhalim <abdelhalimpro@gmail.com>
2022-02-19 13:39:20 +0100

    make age auto calculated

--- src/main/java/com/hrpro/learnspringBoot/student/StudentConfig.java
+++ src/main/java/com/hrpro/learnspringBoot/student/StudentConfig.java
@@ -16,15 +16,13 @@ public class StudentConfig {
             Student ahmed = new Student(
                     "ahmed",
                     "ahmed9@example.com",
-                    LocalDate.of(1995, Month.JUNE, 23),
-                    28
+                    LocalDate.of(1995, Month.JUNE, 23)
             );
 
             Student sara = new Student(
                     "sara",
                     "sara@example.com",
-                    LocalDate.of(1998, Month.APRIL, 4),
-                    23
+                    LocalDate.of(1998, Month.APRIL, 4)
             );
 
             studentRepository.saveAll(List.of(ahmed, sara));
